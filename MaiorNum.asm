7078a8f9433ea4ba909a2120e48e12dff0ee2131
zNidhoggr <50452498+zNidhoggr@users.noreply.github.com>
2023-06-14 09:43:19 -0300

    Add files via upload

--- MaiorNum.asm
+++ MaiorNum.asm
@@ -6,7 +6,7 @@ nl: .asciiz "\n"
 
 .text
 main:
-    la $s0, array       	# carrega o endereço do primeiro elemento da lista em $s0
+    li $s0, 0x10000000       	# carrega o endereço do primeiro elemento da lista em $s0
     lw $t0, ($s0)         	# carrega o primeiro número em $t0
     addi $t1, $zero, 9 		# comprimento da lista
     addi $s0, $s0, 4    	# aponta para o segundo elemento da lista
@@ -15,7 +15,12 @@ loop1:
     lw $t2, ($s0)       	# carrega o próximo número em $t2
     bge $t0, $t2, loop2     	# compara com o maior número atual
     move $t0, $t2       	# se for maior, atualiza o maior número
+    
 loop2:
+    li $v0, 1           	# número de sistema para escrever na tela
+    move $a0, $t0       	# carrega o maior número em $a0
+    syscall
+    
     addi $t1, $t1, -1   	# decrementa o comprimento da lista
     addi $s0, $s0, 4    	# aponta para o próximo elemento da lista
     j loop1         		# continua no loop
